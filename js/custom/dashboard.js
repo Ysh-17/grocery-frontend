dafd348be9d2c473f108c5fe3f180e92a1250b62
Mamidipalli Sri vyshnavi <151949676+Ysh-17@users.noreply.github.com>
2025-12-08 18:23:32 +0530

    dashboard.js

--- js/custom/dashboard.js
+++ js/custom/dashboard.js
@@ -10,7 +10,7 @@ $(function () {
                     '<td>'+ order.datetime +'</td>'+
                     '<td>'+ order.order_id +'</td>'+
                     '<td>'+ order.customer_name +'</td>'+
-                    '<td>'+ order.order_details+'</td>'+
+                    '<td>'+ order.order_details +'</td>'+
                     '<td>'+ order.total.toFixed(2) +' Rs</td></tr>';
             });
             table += '<tr><td colspan="3" style="text-align: end"><b>Total</b></td><td><b>'+ totalCost.toFixed(2) +' Rs</b></td></tr>';
