1925f68c35dc0584a08faa65818f5eafd95e312d
Mamidipalli Sri vyshnavi <151949676+Ysh-17@users.noreply.github.com>
2025-12-08 18:30:38 +0530

    dashboard.js

--- js/custom/dashboard.js
+++ js/custom/dashboard.js
@@ -1,18 +1,29 @@
 $(function () {
-    //Json data by api call for order table
+
+    function formatOrderDetails(details) {
+        if (!details || !Array.isArray(details)) return '';
+
+        return details.map(function(item) {
+            return item.product_name + " (" + item.quantity + " x " + item.price_per_unit + ")";
+        }).join("<br>");
+    }
+
     $.get(orderListApiUrl, function (response) {
         if(response) {
             var table = '';
             var totalCost = 0;
+
             $.each(response, function(index, order) {
                 totalCost += parseFloat(order.total);
+
                 table += '<tr>' +
                     '<td>'+ order.datetime +'</td>'+
                     '<td>'+ order.order_id +'</td>'+
                     '<td>'+ order.customer_name +'</td>'+
-                    '<td>'+ order.order_details +'</td>'+
+                    '<td>'+ formatOrderDetails(order.order_details) +'</td>'+
                     '<td>'+ order.total.toFixed(2) +' Rs</td></tr>';
             });
+
             table += '<tr><td colspan="3" style="text-align: end"><b>Total</b></td><td><b>'+ totalCost.toFixed(2) +' Rs</b></td></tr>';
             $("table").find('tbody').empty().html(table);
         }
